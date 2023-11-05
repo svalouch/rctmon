2a767e3222fcea8aee9ae70025d41faeb44eae16
Stephan Fudeus <sfudeus@users.noreply.github.com>
2023-11-05 16:06:08 +0100

    Improve resilience after disconnect

--- src/rctmon/mqtt.py
+++ src/rctmon/mqtt.py
@@ -8,22 +8,32 @@ from .config import MqttConfig
 from prometheus_client.core import REGISTRY, Sample, Metric
 import logging
 
+log = logging.getLogger(__name__)
+
 
 class MqttClient():
+
+    is_connected: bool
+    conf: MqttConfig
+    topic_prefix: str
+    mqtt_client: mqtt.Client
 
     def __init__(self, mqtt_config: MqttConfig):
         self.conf = mqtt_config
         self.is_connected = False
         self.topic_prefix = [self.conf.topic_prefix.strip("/")]
-        self.mqtt_client = self._connect()
+        self.mqtt_client = self._setup_client()
+        self._connect()
 
-    def _connect(self) -> 'mqtt.Client':
+    def _setup_client(self) -> mqtt.Client:
         mqtt_client = mqtt.Client(client_id=self.conf.client_name)
         mqtt_client.enable_logger()
-        self.on_connect = self.__cb_on_connect
+        mqtt_client.on_connect = self.__cb_on_connect
+        mqtt_client.on_disconnect = self.__cb_on_disconnect
 
         if self.conf.auth_user and self.conf.auth_pass:
-            mqtt_client.username_pw_set(self.conf.auth_user, self.conf.auth_pass)
+            mqtt_client.username_pw_set(
+                self.conf.auth_user, self.conf.auth_pass)
         if self.conf.tls_enable:
             mqtt_client.tls_set(
                 self.conf.tls_ca_cert,
@@ -31,17 +41,44 @@ class MqttClient():
                 self.conf.tls_keyfile
             )
             mqtt_client.tls_insecure_set(self.conf.tls_insecure)
-        mqtt_client.connect(self.conf.mqtt_host, self.conf.mqtt_port)
-        # while self.is_connected == False:
-        #     sleep(1)
+
+        mqtt_client.loop_start()
+
         return mqtt_client
 
+    def _connect(self):
+        log.info("MQTT reconnecting")
+        if self.is_connected:
+            log.warn("MQTT already connected, skipping reconnect")
+        else:
+            self.mqtt_client.connect(self.conf.mqtt_host, self.conf.mqtt_port)
+
     def __cb_on_connect(self, mqttc, obj, flags, rc):
-        self.is_connected =  True
+        log.debug("Received onConnect callback")
+        if rc == 0:
+            log.info("MQTT connection established")
+            self.is_connected = True
+        else:
+            log.warning("Couldn't connect to mqtt because of rc %d", rc)
+
+    def __cb_on_disconnect(self, mqttc, obj, rc):
+        log.debug("Received onDisconnect callback")
+        self.is_connected = False
+
+    def publish(self, topic, payload):
+        if self.is_connected:
+            self.mqtt_client.publish(
+                topic=topic, payload=payload, retain=self.conf.mqtt_retain)
+        else:
+            log.warn("MQTT not connected, skipping publishing")
 
     def flush(self):
         """Flush all metrics from the registry to the mqtt server."""
         ignored_labels = ('inverter')  # ignore the generic inverter label
+
+        log.debug("Flushing metrics")
+        if not self.is_connected:
+            self._connect()
 
         metric: Metric = None
         sample: Sample = None
@@ -51,8 +88,8 @@ class MqttClient():
                 # ignore all additional non-functional metrics
                 continue
 
-
-            base_topic = "/".join(self.topic_prefix + (metric.name.split("_")[1:]))
+            base_topic = "/".join(self.topic_prefix +
+                                  (metric.name.split("_")[1:]))
             for sample in metric.samples:
                 topic = base_topic
                 for label in sample.labels.keys():
@@ -62,5 +99,4 @@ class MqttClient():
                         segment = "{}_{}".format(label, sample.labels[label])
                         topic += "/" + segment
 
-                self.mqtt_client.publish(
-                    topic=topic, payload=sample.value, retain=self.conf.mqtt_retain)
+                self.publish(topic=topic, payload=sample.value)
